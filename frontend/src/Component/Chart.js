e9f40ee4738ecf37a05a4b1e42d55c256e9e3e6d
suryank <suryanksingh.123@gmail.com>
2023-09-16 02:25:03 +0530

    Chart

--- frontend/src/Component/Chart.js
+++ frontend/src/Component/Chart.js
@@ -1,10 +1,31 @@
 import "./css/Chart.css"
+// import {useState} from"react"
+
 
 function Chart() {
+
+    // var today = new Date(),
+    // date = today.getDate() + '/ ' + today.getMonth() + '/' + today.getFullYear()
+    // const [startDate, setStartDate] = useState(date)
+    // const [endDate, setEndDate] = useState(date)
+
     return(
         <div className="chart-section">
-            <div className="main-chart"><div></div></div>
-            <div className="chart-control"></div>
+            <div className="main-chart">
+                    <select>
+                        <option value='candel-stick'>Candel Stick</option>
+                        <option value='line'>Line Graph</option>
+                        <option value='Bar'>Bar Graph</option>
+                    </select>
+                </div>
+            <div className="chart-control">
+                <form>
+                    <label>
+                        From <input type="date" />
+                        To <input type="date" />
+                    </label>
+                </form>
+            </div>
         </div>
     )
 }
--- frontend/src/Component/WatchList.js
+++ frontend/src/Component/WatchList.js
@@ -4,6 +4,15 @@ function WatchList() {
     return(
         <div className="WatchList-Table">
             <table>
+                <thead>
+                    <tr>
+                        <th>Ticker</th>
+                        <th>Open</th>
+                        <th>Close</th>
+                        <th>High</th>
+                        <th>Low</th>
+                    </tr>
+                </thead>
             </table>
         </div>
     )
